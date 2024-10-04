dc1c6e82eecb4d2d0f35f41230dbdd74559f9a46
inno-cent <ekweozorinnocent@gmail.com>
2024-10-04 12:53:02 +0100

    feat : header update

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -5,7 +5,29 @@ const Header = () => {
     <div>
       <header>
         <div className="header--inner">
-          
+          <div className="edw34rf">
+            <div className="vner234f">
+              <div className="toolbar__text">
+                <div className="toolbar__text__frame">
+                  <div className="vgsdfe">
+                    <div className="announcement__text">
+                      <div className="ter234">
+                        <a href="/" className="navlink ">
+                          Theme Features
+                        </a>
+                        <a href="/" className="navlink ">
+                          Blog
+                        </a>
+                        <a href="/" className="navlink ">
+                          FAQ
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
     </div>
@@ -13,3 +35,10 @@ const Header = () => {
 };
 
 export default Header;
+position: relative;
+overflow: hidden;
+margin-left: var(--outer-offset);
+margin-right: var(--outer-offset);
+padding-left: var(--outer);
+padding-right: var(--outer);
+line-height: 1.2;
